a4df91e2033b4e68d2b1c1858e27765e376dafeb
Dimitar Dishev <dimityr.dishev@gmail.com>
2014-05-27 23:00:19 +0300

    Tables added, strange auto generated code...

--- ASP/Flights_Service/Default.aspx.designer.cs
+++ ASP/Flights_Service/Default.aspx.designer.cs
@@ -11,23 +11,5 @@ namespace Flights_Service {
     
     
     public partial class Default {
-        
-        /// <summary>
-        /// BtnXmlToDb control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button BtnXmlToDb;
-        
-        /// <summary>
-        /// HLinkInputData control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HLinkInputData;
     }
 }
