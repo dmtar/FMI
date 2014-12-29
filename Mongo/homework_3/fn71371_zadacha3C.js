02b580955e21548eb9f1fc013131007488b670a9
Dimitar Dishev <dimityr.dishev@gmail.com>
2014-12-29 16:08:30 +0200

    Final version of third homework

--- Mongo/homework_3/fn71371_zadacha3C.js
+++ Mongo/homework_3/fn71371_zadacha3C.js
@@ -1,4 +1,20 @@
-db.albums.aggregate([
+var musiciansDb = db.getSiblingDB('musicians');
+/*
+Add rating property to every album
+ */
+musiciansDb.albums.find().forEach(function(doc) {
+	var random = Math.floor((Math.random() * 100) + 1);
+	db.albums.update({ _id: doc._id },{ $set: { rating:  random} } );
+});
+
+/*
+Subtask 1 
+За всеки жанр на албум да се намери броят албуми в които се среща, средният
+им рейтинг и списък на имената им. Да се запишат в друга колекция трите
+най­често използвани жанра. Записването на резултатите трябва да става като
+част от заявката ­ не на отделна стъпка.
+ */
+musiciansDb.albums.aggregate([
 	{$unwind: "$genres"},
 	{$group: {_id:"$genres", count: {$sum: 1}, avgRating: {$avg: "$rating"}}},
 	{$sort: {count: -1}},
@@ -6,11 +22,13 @@ db.albums.aggregate([
 	{$out: "resultCollection"}
 ]);
 
-db.albums.find().forEach(function(doc) {
-	var random = Math.floor((Math.random() * 100) + 1);
-	db.albums.update({ _id: doc._id },{ $set: { rating:  random} } );
-});
-
+/*
+Subtask 2
+Да се върнат имената на изпълнителите, като се групират по това кой е
+по­вероятно да ги слуша според годината им на създаване. Т.е. да се върнат
+изпълнителите които е по­вероятно да бъдат слушани от “Yuriy” (преди 1968 (1995)),
+“Georgi1” (между 1969(1995) и 1987(2005)) или “Georgi2” (от 1988(2005) нататък). 
+ */
 var mapFunction = function() {
 	if(this.founded < 1995) {
 		emit("Yuriy", this.name);
@@ -25,7 +43,7 @@ var reduceFunction = function(key, values) {
 	return values.join(",");
 };
 
-db.runCommand({
+musiciansDb.runCommand({
 	mapReduce: "artists",
 	map: mapFunction,
 	reduce: reduceFunction,
